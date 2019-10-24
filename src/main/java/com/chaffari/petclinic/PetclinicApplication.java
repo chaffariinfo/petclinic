ddae36cc4852a2f1f675a08379bfdcf518127693
Chaffari Riadh <leaderchaffari@hotmail.com>
2019-10-24 11:20:13 +0100

    Initial commit

---
+++ src/main/java/com/chaffari/petclinic/PetclinicApplication.java
@@ -0,0 +1,20 @@
+package com.chaffari.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * PetClinic Spring Boot Application.
+ *
+ * @author Chaffari Riadh
+ *
+ *
+ */
+@SpringBootApplication
+public class PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicApplication.class, args);
+    }
+
+}
